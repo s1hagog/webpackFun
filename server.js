8bafd182e478bfc77f1ee88c631b7cc18761e30e
Aleksandr Moshak <moshakalex@gmail.com>
2019-10-13 18:46:17 -0700

    development chapter

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require ('express');
+const webpack = require ('webpack');
+
+const webpackDevMiddleware = require('webpack-dev-middleware');
+
+const app = express();
+const config = require('./webpack.config.js');
+const compiler = webpack(config);
+
+app.use(webpackDevMiddleware(compiler, {
+    publicPath: config.output.publicPath
+}))
+
+app.listen(3000, () => {
+    console.log('Example of application listening on port 3000!\n')
+})
